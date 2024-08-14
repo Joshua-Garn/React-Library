a18f5af69ee8997f542f12743bc72cad96c868d1
Joshua-Garn <jgarn02@gmail.com>
2024-08-13 22:18:05 -0600

    Finished the dynamic parts to the cart page for the total prices.

--- App.js
+++ App.js
@@ -12,7 +12,25 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    setCart([...cart, book])
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+    console.log("removeItem", item);
   }
 
   useEffect(() => {
@@ -27,9 +45,21 @@ function App() {
         <Route path="/books" exact render={() => <Books books={books} />} />
         <Route
           path="/books/:id"
-          render={() => <BookInfo books={books} addToCart={addToCart} />}
+          render={() => (
+            <BookInfo cart={cart} books={books} addToCart={addToCart} />
+          )}
         />
-        <Route path="/cart" render={() => <Cart books={books} cart={cart} />} />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          )}
+        />
         <Footer />
       </div>
     </Router>
